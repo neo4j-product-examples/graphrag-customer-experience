e9415b5996771f7578b2f833e4412df19f2a2ab5
zach-blumenfeld <zach.blumenfeld@neo4j.com>
2024-05-15 18:06:12 -0400

    Updating Search UI for Better Presentation

--- ui/pages/1_search.py
+++ ui/pages/1_search.py
@@ -10,7 +10,6 @@ st.markdown(":gray[Similar to Discovery, "
             "GraphRAG integrates customer purchasing patterns with "
             "unstructured product descriptions for "
             "personalized semantic search.]")
-st.markdown('__Product Search:__')
 
 customer_examples = [
     [
@@ -41,44 +40,49 @@ customer_examples = [
 
 remote_runnable = RemoteRunnable("http://api:8080/search/")
 
-with st.form('input_form'):
-    with st.expander('User Context'):
-        preset_example = st.selectbox("select an example case:", customer_examples)
-        customer_name_input = st.text_input("customer name:", value=preset_example[0], key='customer_name_input')
-        customer_id_input = st.text_input("customerId:", value=preset_example[2], key='customer_id_input')
-        time_of_year_input = st.text_input("time of year:", value=preset_example[3], key='time_of_year_input')
+col1, col2 = st.columns([0.35, 0.65])
 
-    customer_interests_input = st.text_input("Search for products:", value=preset_example[1],
-                                             key='customer_interests_input')
-    gen_content = st.form_submit_button('search')
+with col1:
+    st.markdown('#### Product Search:')
+    with st.form('input_form'):
+        with st.expander('User Context'):
+            preset_example = st.selectbox("select an example case:", customer_examples)
+            customer_name_input = st.text_input("customer name:", value=preset_example[0], key='customer_name_input')
+            customer_id_input = st.text_input("customerId:", value=preset_example[2], key='customer_id_input')
+            time_of_year_input = st.text_input("time of year:", value=preset_example[3], key='time_of_year_input')
 
-n_cards_per_row = 2
-if gen_content:
-    search_results = remote_runnable.invoke({
-        'customer_interests': customer_interests_input,
-        'time_of_year': time_of_year_input,
-        'customer_id': customer_id_input
-    })
-    for k in range(len(search_results)):
-        row = search_results[k]
-        i = k % n_cards_per_row
-        if i == 0:
-            st.write("---")
-            cols = st.columns(n_cards_per_row)
-        # draw the card
-        with cols[k % n_cards_per_row]:
-            with st.container(height=300):
-                product_page_url = 'http://localhost:8501/recommendations?' + \
-                                   '&'.join([f'product_code={row["productCode"]}',
-                                             f'customer_name={quote_plus(customer_name_input)}',
-                                             f'interests={quote_plus(customer_interests_input)}',
-                                             f'time_of_year={quote_plus(time_of_year_input)}'
-                                             ])
-                st.markdown(f"## [{row['prodName']}]({product_page_url})")
-                image_urls = []
-                for article in row["articleVariants"][:3]:
-                    image_urls.append(
-                        f'https://storage.cloud.google.com/neo4j-app-images/hm-articles/images/{article["articleId"]}.jpg')
-                st.image(image_urls, width=70)
-                st.markdown(f"{row['detailDesc']}")
+        customer_interests_input = st.text_input("search prompt:", value=preset_example[1],
+                                                 key='customer_interests_input')
+        gen_content = st.form_submit_button('search')
+
+with col2:
+    n_cards_per_row = 2
+    if gen_content:
+        st.write("#### Results: ")
+        search_results = remote_runnable.invoke({
+            'customer_interests': customer_interests_input,
+            'time_of_year': time_of_year_input,
+            'customer_id': customer_id_input
+        })
+        for k in range(len(search_results)):
+            row = search_results[k]
+            i = k % n_cards_per_row
+            if i == 0:
+                cols = st.columns(n_cards_per_row)
+            # draw the card
+            with cols[k % n_cards_per_row]:
+                with st.container(height=400):
+                    product_page_url = 'http://localhost:8501/recommendations?' + \
+                                       '&'.join([f'product_code={row["productCode"]}',
+                                                 f'customer_name={quote_plus(customer_name_input)}',
+                                                 f'interests={quote_plus(customer_interests_input)}',
+                                                 f'time_of_year={quote_plus(time_of_year_input)}'
+                                                 ])
+                    st.markdown(f"## [{row['prodName']}]({product_page_url})")
+                    image_urls = []
+                    for article in row["articleVariants"][:3]:
+                        image_urls.append(
+                            f'https://storage.cloud.google.com/neo4j-app-images/hm-articles/images/{article["articleId"]}.jpg')
+                    st.image(image_urls, width=70)
+                    st.markdown(f"{row['detailDesc']}")
 
