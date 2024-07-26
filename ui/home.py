d90bf976e86f47fbffa77a878b422762af48d7e5
zach-blumenfeld <zach.blumenfeld@neo4j.com>
2024-07-25 18:56:49 -0700

    Parameterize host url for UI

--- ui/home.py
+++ ui/home.py
@@ -1,10 +1,14 @@
+import os
+
 import streamlit as st
+
+ADVERTISED_ADDRESS = os.getenv('ADVERTISED_ADDRESS')
 
 st.title("GenAI for Customer Experience")
 st.subheader("Example Applications Built With Neo4j")
-st.markdown('''
-1. __[Discovery](http://localhost:8501/discovery)__: Improve click-through rate with personalized marketing
-2. __[Search](http://localhost:8501/search)__: Increase conversion with tailored semantic search
-3. __[Recommendations](http://localhost:8501/recommendations)__: Boost average order value with customized recommendations
-4. __[Support](http://localhost:8501/support)__: Reduce cost to serve with well-grounded, fact-based, AI scripts
+st.markdown(f'''
+1. __[Discovery]({ADVERTISED_ADDRESS}:8501/discovery)__: Improve click-through rate with personalized marketing
+2. __[Search]({ADVERTISED_ADDRESS}:8501/search)__: Increase conversion with tailored semantic search
+3. __[Recommendations]({ADVERTISED_ADDRESS}:8501/recommendations)__: Boost average order value with customized recommendations
+4. __[Support]({ADVERTISED_ADDRESS}:8501/support)__: Reduce cost to serve with well-grounded, fact-based, AI scripts
 ''')
